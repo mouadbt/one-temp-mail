4227aeee9c62732eff89dcd4d22cd5c685cf3f64
mouadbt <160415648+mouadbt@users.noreply.github.com>
2026-03-08 10:15:53 +0000

    feat: initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 import "./App.css";
 
 function App() {
   return (
     <>
       <h1 className="text-center bg-yellow-500">Hello</h1>
+      <Button>Yo What's good</Button>
     </>
   );
 }
