8ad9f7692b47332d2a05660378379ef7b4badfe5
mouadbt <160415648+mouadbt@users.noreply.github.com>
2026-08-25 22:11:02 +0100

    fix: pause deplymeent to netlify temporarily on every commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <>
       <h1 className="text-center h-screen flex items-center justify-center">
-        We're currently working behind the scenes to make things better.
+        We're currently working behind the scenes to make things better!
       </h1>
     </>
   );
